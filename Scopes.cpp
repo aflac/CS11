08338db2446389942f6e96ca4f4b0fa81e0e0865
Carlos <jaylink98@gmail.com>
2025-02-20 12:20:30 -0800

    Create Scopes.cpp

---
+++ Scopes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int num = 20;
+    
+    if(true) {
+        cout << "Changing num to 5." << endl;
+        int num = 5;
+    }
+    cout <<"Num is" << num << endl;
+}
