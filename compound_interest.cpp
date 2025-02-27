150fc089a99e2e2a89b1761358a235393ee72d5b
Carlos <jaylink98@gmail.com>
2025-02-27 13:28:01 -0800

    Update compound_interest.cpp

--- compound_interest.cpp
+++ compound_interest.cpp
@@ -5,16 +5,19 @@ using namespace std;
 int main() {
     int year = 2025;
 
-    double salary = 58000;
-    double balance = 0;
-    const double TARGET = 1000000000;
-    //cout << fixed << setprecision(2);
+    //double salary = 58000;
+    double balance = 2000;
+    const double TARGET = balance * 2;
+    const double INTEREST_RATE = 0.04;
 
+    //cout << fixed << setprecision(2);
     // TODO: update the loop to stop when we're a billionaire
 
-    while(balance < TARGET){  
+    while(balance < TARGET){
        year = year +1;  
-       balance = balance + salary;
-       cout << "year:"<< year << "balance: " << balance << endl;
+       balance = balance + (balance * INTEREST_RATE);
+       
+      // balance = balance + salary;
+       cout <<"Year: " << year<< "  " << "Balance: " << balance << endl;
     }
 }
