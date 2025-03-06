f0df91ee15f8b67142885c1b0b68bf4421362561
Carlos <jaylink98@gmail.com>
2025-03-06 13:28:38 -0800

    Update for_loop_exercises.cpp

--- for_loop_exercises.cpp
+++ for_loop_exercises.cpp
@@ -9,7 +9,25 @@ int main() {
 
         cout << text[i] << endl;
 
+        #include <iostream>
+
+using namespace std;
+
+int main() {
+    
+
+    int sum = 0;
+    int total = 0;
+    // Calculate the sum of all squares between 1 and 100
+    
+    for (int i = 1; i <= 100; i = i + 1 ){
+        sum = i * i;
+        total = total + sum;
         
+    }
+    cout << total << endl;
+
+}
     }
 
 }
