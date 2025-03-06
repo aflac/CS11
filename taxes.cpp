349104e842c432a2779096855982b27288f5c635
Carlos <jaylink98@gmail.com>
2025-03-06 13:29:30 -0800

    Update taxes.cpp

--- taxes.cpp
+++ taxes.cpp
@@ -9,9 +9,22 @@ int main() {
     cin >> GROSS_INCOME;
 
     double STD_DEDUCTION = 14600;
+
+    double B_1_HIGH = 11600;
+    double B_2_HIGH = 47150;
     double TAXABLE_INCOME = (GROSS_INCOME-STD_DEDUCTION);
     double TAXES_OWED;
 
- 
+    if(TAXABLE_INCOME < B_1_HIGH){
+
+        TAXES_OWED = (TAXABLE_INCOME * 0.10);
+
+    }
+    else if (TAXABLE_INCOME > B_1_HIGH && TAXABLE_INCOME< B_2_HIGH)
+    {
+        /* code */
+    }
+    
+
     cout << TAXES_OWED <<endl;
 }
