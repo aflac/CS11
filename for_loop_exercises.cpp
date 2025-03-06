ecdd3e1891ae67a928fba7a2a9d4ffc9ab8048e8
Carlos <jaylink98@gmail.com>
2025-03-06 12:26:30 -0800

    Create for_loop_exercises.cpp

---
+++ for_loop_exercises.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string text = "hello";
+
+    for (int i = 0; i<= text.length(); i = i + 1) {
+
+        cout << text[i] << endl;
+
+        
+    }
+
+}
