fa09d4b869a8e69f26b871924a40df11ab8b2f67
Carlos <jaylink98@gmail.com>
2025-03-06 10:29:20 -0800

    Create taxes.cpp

---
+++ taxes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+ cout << "Enter decimal input for your Gross income ";
+    double GROSS_INCOME;
+    cin >> GROSS_INCOME;
+
+    double STD_DEDUCTION = 14600;
+    double TAXABLE_INCOME = (GROSS_INCOME-STD_DEDUCTION);
+    double TAXES_OWED;
+
+
+
+
+
+
+    cout << TAXES_OWED <<endl;
+}
