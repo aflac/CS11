f6976452fbc1fb130dcd0f657534d48bd57fa08d
Carlos <jaylink98@gmail.com>
2025-03-13 12:47:45 -0700

    Update input_statistics.cpp

--- input_statistics.cpp
+++ input_statistics.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
-    double number;
-    int count = 0;
-    double sum = 0;
-
-    //TODO(class exercise : find the maximum number  and print out max)
-    while(cin>> number) {
-        cout << "You Typed: "<< number << endl;
-        sum = sum + number;
-        count = count + 1;
-    }
+double number;
+double sum = 0;
+int count = 0;
+double curr_max = -9999999999;
+string name;
+string person_high_score;
 
 
-        if(count > 0){
-                cout << "Average" << sum / count << endl;
+// TODO (class exercise): Find the maximum number.
+while(cin >> name >> number) {
 
-        }
+ cout << "You typed: " << number << endl;
+
+
+ if(number > curr_max){
+    curr_max = number;
+    person_high_score = name;
+
+}
+sum = sum + number;
+count = count + 1; // count++
+
+
+
+if (count > 0) {
+cout << "Average: " << sum / count << endl;
+cout << "Max is: " << curr_max << endl;
+}
+}
+
 }
