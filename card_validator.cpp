b3fbe7b271009936a58d1fb97420086d7a53fe82
Carlos <jaylink98@gmail.com>
2025-03-19 13:02:37 -0700

    Update card_validator.cpp

--- card_validator.cpp
+++ card_validator.cpp
@@ -4,6 +4,36 @@ using namespace std;
 
 int main() {
 
+    long n ;
+    cin >> n;
+    int sum = 0;
+    int count = 0;
+    int digit;
+    while(n > 0){
+         digit = n % 10; // Grab last digit.
+        n = n / 10;         // Cut off the last digit.      
 
+        if(count % 2 == 0) {
+            sum = sum + digit;
 
+        }
+        else if(count % 2 == 1){
+            digit = digit * 2;
+
+            if(digit > 9){
+                digit = digit - 9;
+            }
+            sum = sum + digit;
+        }
+         count++;
+
+    }
+
+         if(sum % 10 == 0){
+
+        cout << "Valid" << endl;
+    }
+        else{
+            cout<< "Invalid" << endl;
+        }
 }
